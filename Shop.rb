96732d2cf16666c29f33e8404be3ff5eddd94351
GO-Shoderu <go.shoderu@gmail.com>
2022-11-01 00:21:21 +0200

    📝 Added the complete implementation of the Shop system

--- Shop.rb
+++ Shop.rb
@@ -14,10 +14,11 @@ class StockItem
     end
 
     def price
+        return @price
     end
 end
 
-class Shop < StockItem
+class Shop
     attr_accessor :stockItems
 
     def initialize
@@ -50,16 +51,46 @@ class Shop < StockItem
     end
 
     def print()
+        puts "\nItems:"
         for stockItem in stockItems
-            puts "Stock name: #{stockItem.name}"
-            puts "Stock price: #{stockItem.price}"
-            puts "\n"
+            puts "#{stockItem.name}: #{stockItem.price}"
         end
+    end
+end
+
+class NormalPriceStockItem < StockItem
+    def price
+        return @price
+    end
+end
+
+class DiscountedStockItem < StockItem
+    def price
+        return @price - (0.1 * @price)
     end
 end
 
 my_shop = Shop.new()
 
-my_shop.addStockItem(StockItem.new(), "Pencil")
-my_shop.setPrice("Pencil", 15)
+for index in 1..3
+    print "\nEnter name for item #{index}: "
+    name = gets.chomp
+
+    print "Enter price for item #{index}: "
+    price = gets.chomp
+
+    print "Enter type of item  #{index}: "
+    type = gets.chomp
+
+    if type == "n"
+        my_shop.addStockItem(NormalPriceStockItem.new(), name)
+        my_shop.setPrice(name.to_s, price.to_f)
+    elsif type == "d"
+        my_shop.addStockItem(DiscountedStockItem.new(), name)
+        my_shop.setPrice(name.to_s, price.to_f)
+    end
+end
+
 my_shop.print()
+
+puts "\nAverage price is #{(my_shop.getAveragePrice()).round(2)}"
